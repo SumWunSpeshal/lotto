c83e00b86350edc17c816778d5b19c5625e71c61
Sacha Rodier <sacharodier94@googlemail.com>
2020-09-29 19:19:09 +0200

    finalizing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import Ball from './Ball';
 import Field from './Field';
 
+const STORAGE_SERVICE = 'combination';
+
 class App extends React.Component {
+  componentDidMount() {
+    const prevCombination = JSON.parse(localStorage.getItem(STORAGE_SERVICE));
+    this.setState({
+      combination: prevCombination.length ? prevCombination : [],
+      selectionComplete: prevCombination.length >= 6,
+    });
+  }
+
   nums = Array(49)
     .fill(0)
     .map((item, index) => index + 1);
@@ -12,21 +22,37 @@ class App extends React.Component {
     selectionComplete: false,
   };
 
-  submit() {
-    console.log(this.state.combination);
+  async submit() {
+    const body = { combination: this.state.combination };
+
+    console.log(body); // result
+
+    const response = await fetch('https://www.google.com', {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      body: JSON.stringify(body),
+    });
+
+    return response.json();
   }
 
   getItemValue(value) {
     let newArr = this.state.combination;
     this.state.combination.indexOf(value) !== -1 ? newArr.splice(newArr.indexOf(value), 1) : newArr.push(value);
     this.setState({ combination: newArr, selectionComplete: newArr.length >= 6 });
+    localStorage.setItem(STORAGE_SERVICE, JSON.stringify(newArr));
+  }
+
+  delete() {
+    this.setState({ combination: [], selectionComplete: false });
   }
 
   render() {
     return (
-      <div className='App grid-container'>
+      <div className='App grid-container margin-top-3'>
         <div className='grid-x grid-margin-x'>
-          <div className='cell small-5'>
+          <div className='cell small-9'>
             <section className='board'>
               <ul className='board__list'>
                 {this.nums.map(item => (
@@ -34,22 +60,36 @@ class App extends React.Component {
                     key={item}
                     value={item}
                     isDisabled={this.state.selectionComplete}
+                    isActive={this.state.combination.indexOf(item) !== -1}
                     eventHandler={val => this.getItemValue(val)}
                   />
                 ))}
               </ul>
             </section>
-            <button
-              type='button'
-              onClick={() => this.submit()}
-              className={`button step-${this.state.combination.length} ${
-                this.state.selectionComplete ? '' : ' is-disabled'
-              }`}
-            >
-              Submit
-            </button>
+            <div className='grid-x grid-margin-x'>
+              <div className='cell small-6'>
+                <button
+                  type='button'
+                  onClick={() => this.delete()}
+                  className={`button button--delete ${this.state.combination.length ? '' : ' is-disabled'}`}
+                >
+                  <span>Delete all</span>
+                </button>
+              </div>
+              <div className='cell small-6'>
+                <button
+                  type='button'
+                  onClick={() => this.submit()}
+                  className={`button step-${this.state.combination.length} ${
+                    this.state.selectionComplete ? '' : ' is-disabled'
+                  }`}
+                >
+                  <span>Submit</span>
+                </button>
+              </div>
+            </div>
           </div>
-          <div className='cell small-7'>
+          <div className='cell small-3'>
             <div className='balls'>
               {this.state.combination.map(item => (
                 <Ball key={item} num={item} />
--- src/Field.js
+++ src/Field.js
@@ -1,41 +1,30 @@
 import React from 'react';
 
-class Field extends React.Component {
-  state = {
-    active: false,
-  };
-
-  onClick = () => {
-    this.props.eventHandler(this.props.value);
-    this.setState({ active: !this.state.active });
-  };
-
-  render() {
-    return (
-      <li className='board__item'>
-        <button
-          onClick={this.onClick}
-          className={
-            'board__button' +
-            (this.state.active ? ' is-active' : '') +
-            (this.props.isDisabled && !this.state.active ? ' is-disabled' : '')
-          }
-        >
-          {this.props.value}
-          <svg className='board__cross' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 111.1785 111.4073'>
-            <path
-              d='M27.4757,123.1172c18.4008-24.6937,43.827-43.1,65.3288-65.1463,13.3595-13.6977,26.2785-29.5267,44.6117-35.0034'
-              transform='translate(-27.4757 -22.9675)'
-            />
-            <path
-              d='M40.9273,26.3983C41.99,31.39,44.436,35.9657,46.96,40.4014a316.2107,316.2107,0,0,0,44.51,60.2629c13.3863,14.2247,28.6535,27.5349,47.1845,33.71'
-              transform='translate(-27.4757 -22.9675)'
-            />
-          </svg>
-        </button>
-      </li>
-    );
-  }
+function Field(props) {
+  return (
+    <li className='board__item'>
+      <button
+        onClick={() => props.eventHandler(props.value)}
+        className={
+          'board__button' +
+          (props.isActive ? ' is-active' : '') +
+          (props.isDisabled && !props.isActive ? ' is-disabled' : '')
+        }
+      >
+        {props.value}
+        <svg className='board__cross' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 111.1785 111.4073'>
+          <path
+            d='M27.4757,123.1172c18.4008-24.6937,43.827-43.1,65.3288-65.1463,13.3595-13.6977,26.2785-29.5267,44.6117-35.0034'
+            transform='translate(-27.4757 -22.9675)'
+          />
+          <path
+            d='M40.9273,26.3983C41.99,31.39,44.436,35.9657,46.96,40.4014a316.2107,316.2107,0,0,0,44.51,60.2629c13.3863,14.2247,28.6535,27.5349,47.1845,33.71'
+            transform='translate(-27.4757 -22.9675)'
+          />
+        </svg>
+      </button>
+    </li>
+  );
 }
 
 export default Field;
